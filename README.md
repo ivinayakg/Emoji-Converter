cc5387780da06cceacd67029b76c968ad7708948
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2021-08-18 17:20:20 +0530

    created app the programming of the whole app is done and is now ready for action

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Emoji-Converter
-Created with CodeSandbox
+
+## This is a very simple app that helps convert emoji into the emotion they stand for.(More like a emoji meaning dictionary)
+
+### Created with react and hosted on code sandbox
--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  const textChangeHandler = (e) => {
+    var value = e.target.value;
+    setMeaning(emoji[value]);
+    setTimeout(() => {
+      e.target.value = "";
+    }, 3000);
+  };
+
+  const onClickHandler = (item) => {
+    setMeaning(emoji[item]);
+  };
+
+  var emoji = {
+    "😍": "Love",
+    "😃": "Smile",
+    "😡": "Anger",
+    "😱": "Shock",
+    "🤢": "Disgusting",
+    "👍": "Best Of Luck",
+    "😣": "Frustrated"
+  };
+
+  var emoji_face = Object.keys(emoji);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Emoji Decoder</h1>
+      <textarea
+        rows="2"
+        cols="30"
+        placeholder="Put your emoji here"
+        onChange={textChangeHandler}
+        maxLength="2"
+        id="input"
+      ></textarea>
+      <h3>
+        Or <br /> Select Them From Below
+      </h3>
+      <ul id="list">
+        {emoji_face.map((item) => {
+          return (
+            <li id="emoji" key={item} onClick={() => onClickHandler(item)}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+      <br />
+      <br />
+      <h2>{meaning}</h2>
     </div>
   );
 }
