c2cdbfe409a7c3a4b4023ae8b2de3b432ebd1590
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2021-08-27 02:07:29 +0530

    styling added footer with syling is added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { useState } from "react";
+import Footer from "./Footer";
 
 export default function App() {
   const [meaning, setMeaning] = useState("");
@@ -29,31 +30,36 @@ export default function App() {
   var emoji_face = Object.keys(emoji);
 
   return (
-    <div className="App">
-      <h1>Emoji Decoder</h1>
-      <textarea
-        rows="2"
-        cols="30"
-        placeholder="Put your emoji here"
-        onChange={textChangeHandler}
-        maxLength="2"
-        id="input"
-      ></textarea>
-      <h3>
-        Or <br /> Select Them From Below
-      </h3>
-      <ul id="list">
-        {emoji_face.map((item) => {
-          return (
-            <li id="emoji" key={item} onClick={() => onClickHandler(item)}>
-              {item}
-            </li>
-          );
-        })}
-      </ul>
-      <br />
-      <br />
-      <h2>{meaning}</h2>
-    </div>
+    <>
+      <header>
+        <h1>Emoji Convertor</h1>
+      </header>
+      <div className="App">
+        <textarea
+          rows="2"
+          cols="30"
+          placeholder="Put your emoji here"
+          onChange={textChangeHandler}
+          maxLength="2"
+          id="input"
+        ></textarea>
+        <h3>
+          Or <br /> Select Them From Below
+        </h3>
+        <ul id="list">
+          {emoji_face.map((item) => {
+            return (
+              <li id="emoji" key={item} onClick={() => onClickHandler(item)}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+        <br />
+        <br />
+        <h2>{meaning}</h2>
+      </div>
+      <Footer />
+    </>
   );
 }
